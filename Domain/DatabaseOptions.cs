59cdec82df9d0ad1f17dc3cfba44d47e100ef959
anatoly8525 <91669406+anatoly8525@users.noreply.github.com>
2022-12-02 21:29:16 +0200

    pad lab3
    lab3

---
+++ Domain/DatabaseOptions.cs
@@ -0,0 +1,13 @@
+﻿namespace Domain
+{
+    public class DatabaseOptions
+    {
+#if DEBUG
+        public const string DatabaseConnectionString = @"Data Source=DESKTOP-7I0RAPV;Initial Catalog=PostMaker;Integrated Security=True;Persist Security Info=False;Pooling=False;MultipleActiveResultSets=False;Connect Timeout=60;Encrypt=False;TrustServerCertificate=False";
+#endif  
+#if RELEASE
+        public const string DatabaseConnectionString = @"Server=tcp:padti.database.windows.net,1433;Initial Catalog=PostMaker;Persist Security Info=False;User ID=pad;Password=utm.lab3;MultipleActiveResultSets=False;Encrypt=True;TrustServerCertificate=False;Connection Timeout=30;";
+#endif 
+
+    }
+}
